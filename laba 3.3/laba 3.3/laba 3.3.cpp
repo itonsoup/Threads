86f848775255f19f82bb36861156ae73f7a1a5a5
itonsoup <145210725+itonsoup@users.noreply.github.com>
2024-05-16 13:43:15 +0300

    Add files via upload

---
+++ laba 3.3/laba 3.3/laba 3.3.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+mutex mtx;
+condition_variable cv;
+int counter = 0; // переменная счетчика должна быть доступна для обоих потоков
+
+void parent_thread()
+{
+    while (true) {
+        unique_lock<mutex> lock(mtx);
+        // Ждем сигнала от дочернего потока о том, что пора выводить строку
+        cv.wait(lock, [] { return counter % 2 == 0; });
+        if (counter >= 10)
+            break;
+        cout << "Line " << counter + 1 << " of the first thread\n";
+        counter++;
+        // Сигнализируем дочернему потоку о том, что пора его пробудить
+        cv.notify_one();
+    }
+}
+
+void child_thread()
+{
+    while (true) {
+        unique_lock<mutex> lock(mtx);
+        // Ждем сигнала от родительского потока о том, что пора выводить строку
+        cv.wait(lock, [] { return counter % 2 == 1; });
+        if (counter >= 10)
+            break;
+        cout << "Line " << counter + 1 << " of the second thread\n";
+        counter++;
+        // Сигнализируем родительскому потоку о том, что пора его пробудить
+        cv.notify_one();
+    }
+}
+
+int main()
+{
+    thread t1(parent_thread);
+    thread t2(child_thread);
+    t1.join();
+    t2.join();
+
+    return 0;
+}
---
+++ laba 3.4/laba 3.4/laba 3.4.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <thread>
+#include <semaphore.h>
+
+using namespace std;
+
+sem_t parent_sem;
+sem_t child_sem;
+int counter = 0; // переменная счетчика должна быть доступна для обоих потоков
+
+void parent_thread()
+{
+    while (true) {
+        // Ждем сигнала от дочернего потока о готовности вывода строки
+        sem_wait(&parent_sem);
+        if (counter >= 10)
+            break;
+        cout << "Line " << counter + 1 << " of the first thread\n";
+        counter++;
+        // Оповещаем дочерний поток о готовности к выводу строки
+        sem_post(&child_sem);
+    }
+}
+
+void child_thread()
+{
+    while (true) {
+        // Ждем сигнала от родительского потока о готовности вывода строки
+        sem_wait(&child_sem);
+        if (counter >= 10)
+            break;
+        cout << "Line " << counter + 1 << " of the second thread\n";
+        counter++;
+        // Оповещаем родительский поток о готовности к выводу строки
+        sem_post(&parent_sem);
+    }
+}
+
+int main()
+{
+    // Инициализируем семафоры
+    sem_init(&parent_sem, 0, 1);
+    sem_init(&child_sem, 0, 0);
+
+    thread t1(parent_thread);
+    thread t2(child_thread);
+    t1.join();
+    t2.join();
+
+    // Уничтожаем семафоры
+    sem_destroy(&parent_sem);
+    sem_destroy(&child_sem);
+
+    return 0;
+}
